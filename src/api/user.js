8ecc776a3dd4a2de91c1ec7a471982a73ab76d9c
moddas311 <moddasir311@gmail.com>
2023-04-15 23:48:17 +0600

    host user sdded

--- src/api/user.js
+++ src/api/user.js
@@ -28,7 +28,23 @@ export const getUserRole = async (email) => {
 // Get all users
 export const getAllUsers = async () => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}/users`);
-  console.log("object");
+  const users = await response.json();
+  return users;
+};
+
+export const makeHost = async (user) => {
+  console.log(user);
+  delete user._id;
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/user/${user?.email}`,
+    {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ ...user, role: "host" }),
+    }
+  );
   const users = await response.json();
   return users;
 };
